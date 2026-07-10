05c2d8c9c49af33d9e11b6b67e5c2142f363305f
dilandavid2 <dilandavid2103@gmail.com>
2026-07-09 20:24:04 -0400

    Conectar frontend con backend en Render

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: "https://casa-cambio-backend-3qsp.onrender.com",
   headers: {
     "x-user-id": "1",
   },
